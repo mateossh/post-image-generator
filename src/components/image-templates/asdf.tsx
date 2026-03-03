d34f17ea39b3e4b82b0e6bca4a5567be31b61ec9
Mateusz Ż <zochowski.mateusz@outlook.com>
2026-03-03 01:45:19 +0100

    tinker with takumi

---
+++ src/components/image-templates/asdf.tsx
@@ -0,0 +1,5 @@
+export default function AsdfTemplate() {
+  return <div tw="w-full h-full flex items-center justify-center bg-green-200 text-black">
+      <h1 tw="text-4xl font-bold text-blue-400">Hello Takumi</h1>
+    </div>
+}
