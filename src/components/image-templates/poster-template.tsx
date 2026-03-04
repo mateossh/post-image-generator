83b9193f3dd697dcc5d1af4a3956882f57cdc10b
Mateusz Ż <zochowski.mateusz@outlook.com>
2026-03-04 13:29:52 +0100

    start polishing template

--- src/components/image-templates/poster-template.tsx
+++ src/components/image-templates/poster-template.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+import type { ReactNode, PropsWithChildren } from "react";
 
 export type TemplateData = {
   content: ReactNode;
@@ -13,15 +13,50 @@ export default function PosterTemplate({ templateData }: Props) {
   const { content, backgroundFile } = templateData;
 
   return (
+    <Container backgroundFile={backgroundFile}>
+      <div tw="h-full flex flex-col justify-end items-center">
+
+        <div tw="text-5xl bg-pink-100 p-4 font-bold text-blue-400">{content}</div>
+
+        <div
+          tw="w-full inline p-4 text-4xl bg-yellow-200"
+          style={{ textWrap: 'pretty' }}
+        >
+          lorem ipsum dolor sit amet
+          <br />
+          lorem ipsum dolor sit amet
+          <br />
+          lorem ipsum dolor sit amet
+          <br />
+          lorem ipsum dolor sit amet
+          <br />
+          lorem ipsum dolor sit amet
+          <br />
+          lorem ipsum dolor sit amet
+          <br />
+
+        </div>
+
+      </div>
+    </Container>
+  );
+}
+
+type ContainerProps = PropsWithChildren<{
+  backgroundFile: string | null
+}>
+
+function Container({ backgroundFile, children }: ContainerProps) {
+  return (
     <div
-      tw="w-full h-full flex items-center justify-center bg-green-200 text-black"
+      tw="w-full h-full flex items-center justify-center bg-green-200 p-16"
       style={{
         backgroundImage: `url(${backgroundFile})`,
         backgroundSize: "cover",
-        backgroundPosition: "center center",
+        backgroundPosition: "center", // NOTE: it doesn't behave correctly
       }}
     >
-      <h1 tw="text-4xl font-bold text-blue-400">{content}</h1>
+      {children}
     </div>
   );
 }
